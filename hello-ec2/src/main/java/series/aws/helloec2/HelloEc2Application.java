e5c58a7dc53dd608ec772ba5cb60441a62183d05
kickscar <kickscar@gmail.com>
2025-12-05 19:45:21 +0900

    init commit

---
+++ hello-ec2/src/main/java/series/aws/helloec2/HelloEc2Application.java
@@ -0,0 +1,23 @@
+package series.aws.helloec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class HelloEc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloEc2Application.class, args);
+    }
+
+    @Controller
+    public static class MvcController {
+        @GetMapping("")
+        public String hello() {
+            return "hello";
+        }
+    }
+
+}
---
+++ hello-ec2/src/test/java/series/aws/helloec2/HelloEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package series.aws.helloec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
